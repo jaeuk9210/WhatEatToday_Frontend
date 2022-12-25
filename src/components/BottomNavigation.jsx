5ed33b2b92ad25129a50b85621ed730498020cf1
정재욱 <jeongjaeuk@Jaeuk-MacBookPro.local>
2022-12-25 15:24:37 +0900

    scroll test

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -5,7 +5,7 @@ import route from "../routes";
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  position: absolute;
+  position: fixed;
   bottom: 0;
   width: 100%;
   height: 55px;
